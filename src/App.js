e872fa9c0a5462d91c1abbc8f3d45dff33c67c19
inyaJohnson <intoajohnson@gmail.com>
2019-12-05 15:36:47 +0100

    Added style for home

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import {Container} from 'react-bootstrap';
 import AppRoute from './components/AppRoute'
 
 function App() {
   return (
-    <Container fluid className="App">
+    <div className="App">
       <AppRoute />
-    </Container>
+    </div>
   );
 }
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,10 @@
 import React, {Component} from 'react';
-import {Row, Col, Form, Image} from 'react-bootstrap';
+import {Container, Row, Col, Form, Figure, Image, InputGroup} from 'react-bootstrap';
 import {NavLink} from 'react-router-dom';
 import { connect } from 'react-redux';
 import fetchGifs from '../components/actions/gifs';
-
+import Search from './search.png';
+import './style.css';
 
 class Home extends Component{
     constructor(){
@@ -25,34 +26,52 @@ class Home extends Component{
     render(){
         const { gifs, fetched } = this.props.gifs;
         return(
-            <Row>
+            <Container fluid className='background'>
                 <Row>
-                    <Col>
-                        <Form>
-                            <Form.Control type='text' onChange={this.handleChange} value={this.state.search}/>    
+                    <Col md={{span:10, offset:1}} >
+                        <h2 className='title'>Riby-Test</h2>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col md={{span:10, offset:1}} className='search-input'>
+                        <Form class='search-form'>
+                            <InputGroup>
+                                <Form.Control size='lg' type='text' onChange={this.handleChange} 
+                                    value={this.state.search} placeholder='Search for GIF' className='search-input-field'/> 
+                                <InputGroup.Append>
+                                    <InputGroup.Text id="inputGroupAppend"><Image src={Search} alt='Search' width='20px' height='20px' /></InputGroup.Text>
+                                </InputGroup.Append>
+                            </InputGroup>
                         </Form>
                     </Col>
                 </Row>
-                {fetched && gifs.length > 0 && (
-                    <Row>
-                        <Col md={12}>
-                            {gifs.map((item, key) =>{
+
+                <Row>
+                    <Col md={{span:10, offset:1}} className='search'>
+                        
+                        {fetched && gifs.length > 0 && (
+                            gifs.map((item, key) =>{
                                 return(
-                                    <span key={key}>
-                                    <NavLink to={`/gif/${item.id}`}>
-                                        <Image src={item.images.downsized.url} alt='gif' width="150" height="150" />
+                                    <span key={key} className='gif'>
+                                        <NavLink to={`/gif/${item.id}`}>
+                                            <Figure>
+                                                <Image src={item.images.downsized.url} alt='gif' className='image'
+                                                    rounded width='180px'  height='150px' />
+                                                <Figure.Caption className='image-caption'>{item.title.split(' ')[0]}</Figure.Caption>
+                                            </Figure>
                                         </NavLink>
                                     </span>
                                 )
                             })
-                        }
-                        </Col> 
-                    </Row>
-                )}
-            </Row>
+                        )}
+                        
+                    </Col>
+                </Row>
+            </Container>
         )
     }
 }
+
 
 const mapStateToProps = state => {
     return {
